e0fa2a822de010b7e039407f52eae223201a0a2d
martproz <martinprozapas@gmail.com>
2022-09-15 13:11:44 -0300

    modify django admin area, #6

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Venue
+from .models import MyClubUser
+from .models import Event
+# El . quiere decir "en el mismo directorio", events/models.py
 
-# Register your models here.
+#admin.site.register(Venue)
+admin.site.register(MyClubUser)
+admin.site.register(Event)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display =  ('name', 'adress', 'phone')
+    ordering = ('name',) #ordena alfabéticamente lqs ve en admin
+    search_fields = ('name', 'address')
--- events/models.py
+++ events/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=120)
+    adress = models.CharField(max_length=300)
+    zip_code = models.CharField('Zip Code', max_length=15)
+    phone = models.CharField('Contact Phone', max_length=25, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_adress = models.EmailField('Email Adress', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class MyClubUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Event(models.Model):
+    name = models.CharField('Events Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.ForeignKey(
+        Venue, blank=True, null=True, on_delete=models.CASCADE)
+    manager = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+    #venue_image = models.ImageField(null=True, blank=True, upload_to='images/')
+    #upload to goes to media directory, settings, media root media, as
+    # soon as we create an image, the media directory will be created automatically
+    # inside that media file will be a subdirectory called images
+    # where all our images will be saved
