eee093c399ea72dadf0522aa3040f1c27e84a093
martproz <martinprozapas@gmail.com>
2022-09-17 12:44:12 -0300

    before query db venues

--- events/forms.py
+++ events/forms.py
@@ -1,16 +1,26 @@
-from socket import fromshare
 from django import forms
 from django.forms import ModelForm
 from .models import Venue
 
-#venue Form
 class VenueForm(ModelForm):
     class Meta:
         model = Venue
         fields = ('name', 'adress', 'zip_code', 'phone', 'web', 'email_adress')
+        labels = {
+            'name': '',
+            'adress': '',
+            'zip_code': '',
+            'phone': '',
+            'web': '',
+            'email_adress': ''
+        }
 
-#1) Creo class en forms
-#2) Creo html add_venue usando la plantilla de home
-#3) Creo el path en urls.py
-#4) Creo views.py
-#5) Necesito crear un link en la navbar
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Venue Name'}),
+            'adress': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Adress'}),
+            'zip_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zip Code'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}),
+            'web': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Web'}),
+            'email_adress': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+        }
+
