ec6db635a2b9f967ede13b9ea70965acdd5e4f3b
martproz <martinprozapas@gmail.com>
2022-09-22 14:57:18 -0300

    bf wk imgs

--- myclub_website/settings.py
+++ myclub_website/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
