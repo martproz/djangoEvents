963271c01826e62862a7dad07ae83481444b3778
MatiMoreno11 <matimoreno11@gmail.com>
2022-09-23 00:20:11 -0300

    ChatUsuarios

--- events/views.py
+++ events/views.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from django.http import HttpResponseRedirect
 from .models import Event, Venue
 from .forms import VenueForm, EventForm
+from .models import Room, Message
+from django.http import HttpResponse, JsonResponse
 
 def about_us(request):
     return render(request, 'events/about_us.html')
@@ -121,3 +123,41 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
         'current_year': current_year,
         'time': time,
     })
+
+def messages(request):
+    return render(request,'events/messages.html')
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'events/room.html', {
+        'username': username,
+        'room': room,
+        'room_details': room_details
+    })
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
